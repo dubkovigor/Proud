a010bbe19dda53d1aeffaf6716bc33262070c213
alexgordeenkov <alexei.gordeenkov@gmail.com>
2019-09-20 15:02:22 +0300

    Starting implementation of Controllers

--- src/main/java/com/solution/controller/EndPointForCar.java
+++ src/main/java/com/solution/controller/EndPointForCar.java
@@ -1,9 +1,16 @@
 package com.solution.controller;
 
+import com.solution.dto.CoordinateDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/car/{carId}")
 public interface EndPointForCar {
+
+    @PostMapping("/currentCoordinates")
+    ResponseEntity<String> setCurrentCoordinates(@RequestParam CoordinateDto coordinateDto);
 }
--- src/main/java/com/solution/controller/impl/EndPointForCarImpl.java
+++ src/main/java/com/solution/controller/impl/EndPointForCarImpl.java
@@ -1,6 +1,14 @@
 package com.solution.controller.impl;
 
 import com.solution.controller.EndPointForCar;
+import com.solution.dto.CoordinateDto;
+import org.springframework.http.ResponseEntity;
 
 public class EndPointForCarImpl implements EndPointForCar {
+
+
+    @Override
+    public ResponseEntity<String> setCurrentCoordinates(CoordinateDto coordinateDto) {
+        return ResponseEntity.ok("Igor pidor");
+    }
 }
